19c01a9a1ea3db7aa367cdfa63bb907b0c04bee1
Lion Yang <lion@aosc.io>
2024-06-17 05:12:06 +0800

    approx: Boltzmann and Gaussian
    p.45

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-#import torch
+import torch
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("TkAgg")
 
-V, n, m, h = 0, 0.3181736168219989, 0.0531348913086363, 0.5875799149114899
+V, n, m, h = -65, 0.3181736168219989, 0.0531348913086363, 0.5875799149114899
 
 C = 1.0
+I = 0
 
 gK, gNa, gL = 36, 120, 0.3
-EK, ENa, EL = -12, 120, 10.6
+EK, ENa, EL = -77, 55, -54.4
 
 emu_rate = 0.001
 min_step = 0.01
 
-t = -10.0
+t = 0
 step = 0
 t_all = []
 V_all = []
+
+def gating_var_inf(voltage: torch.Tensor, half_voltage, slope_factor):
+    return voltage.sub(half_voltage).mul(-1.0 / slope_factor).exp().add(1).reciprocal()
+def gating_var_tau(voltage: torch.Tensor, max_voltage, deviation, tau_base, tau_amp):
+    return voltage.sub(max_voltage).square().mul(-1.0 / deviation ** 2).exp().mul(tau_amp).add(tau_base)
 
 while True:
     print(V, n, m, h)
@@ -36,17 +42,10 @@ while True:
         plt.pause(0.001)
 
     V_dot = I - gK * n ** 4 * (V - EK) - gNa * m ** 3 * h * (V - ENa) - gL * (V - EL)
-
-    alpha_n = lambda V: 0.01 * (10 - V) / (np.exp((10 - V) / 10) - 1)
-    beta_n = lambda V: 0.125 * np.exp(-V / 80)
-    alpha_m = lambda V: 0.1 * (25 - V) / (np.exp((25 - V) / 10) - 1)
-    beta_m = lambda V: 4 * np.exp(-V / 18)
-    alpha_h = lambda V: 0.07 * np.exp(- V)
-    beta_h = lambda V: 1 / (np.exp((30 - V) / 10) + 1)
-
-    n_dot = alpha_n(V) * (1 - n) - beta_n(V) * n
-    m_dot = alpha_m(V) * (1 - m) - beta_m(V) * m
-    h_dot = alpha_h(V) * (1 - h) - beta_h(V) * h
+    Vt = torch.tensor(V)
+    n_dot = gating_var_inf(Vt, -53, 15).sub(n).div(gating_var_tau(Vt, -79, 50, 4.7, 1.1)).item()
+    m_dot = gating_var_inf(Vt, -40, 15).sub(m).div(gating_var_tau(Vt, -38, 30, 0.46, 0.04)).item()
+    h_dot = gating_var_inf(Vt, -62, -7).sub(h).div(gating_var_tau(Vt, -67, 20, 7.4, 1.2)).item()
 
     if t > 50: # np.abs(np.array([V_dot, n_dot, m_dot, h_dot])).max() < min_step:
         break
